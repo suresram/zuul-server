c0b30830052402b621f7d8843b8d33fbf5cce167
Eksprez <admin@eksprez.com>
2019-11-05 22:24:02 +0530

    updated cors header

--- src/main/java/com/s2/spring/cloud/zuul/server/security/CorsFilter.java
+++ src/main/java/com/s2/spring/cloud/zuul/server/security/CorsFilter.java
@@ -23,7 +23,8 @@ public class CorsFilter implements Filter {
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         final HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "*.openshift.opentlc.com");
+        HttpServletRequest request = (HttpServletRequest) req;
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
         response.setHeader("Access-Control-Max-Age", "3600");
