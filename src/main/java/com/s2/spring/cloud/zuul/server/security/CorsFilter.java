c1da22e48064e78f252159bf6386dce9b86f96a0
Eksprez <admin@eksprez.com>
2019-11-05 21:39:39 +0530

    modified response header

--- src/main/java/com/s2/spring/cloud/zuul/server/security/CorsFilter.java
+++ src/main/java/com/s2/spring/cloud/zuul/server/security/CorsFilter.java
@@ -23,7 +23,7 @@ public class CorsFilter implements Filter {
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         final HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "travel-management-ui-travel-management.apps.na311.openshift.opentlc.com");
+        response.setHeader("Access-Control-Allow-Origin", "*.openshift.opentlc.com");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
         response.setHeader("Access-Control-Max-Age", "3600");
