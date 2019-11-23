c3c48a4da1ec082bc3a416b92bbee19d3316ef0d
Eksprez <admin@eksprez.com>
2019-11-23 23:53:29 +0530

    Configured hystrix dashboard

--- src/main/java/com/s2/spring/cloud/zuul/server/ZuulServerApplication.java
+++ src/main/java/com/s2/spring/cloud/zuul/server/ZuulServerApplication.java
@@ -2,12 +2,14 @@ package com.s2.spring.cloud.zuul.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableResourceServer
+@EnableHystrixDashboard
 public class ZuulServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZuulServerApplication.class, args);
